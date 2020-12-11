131781b537745df4db031167875c382af29395c9
Akrith H Nayak <akrithnayak2000@gmail.com>
2020-12-11 20:30:04 +0530

    Added deploy configuration

--- src/Home.js
+++ src/Home.js
@@ -16,10 +16,16 @@ class Home extends Component {
         <h1>Welcome to Algo-Visualiser</h1>
         <h5 style={{ marginTop: "100px" }}>Select any one:</h5>
         <div className="mt-5">
-          <a href="/#/sorting" className="btn btn-outline-light mr-1">
+          <a
+            href="/algo-visualiser/sorting"
+            className="btn btn-outline-light mr-1"
+          >
             Sorting visualization
           </a>
-          <a href="/#/pathfinding" className="btn btn-outline-light">
+          <a
+            href="/algo-visualiser/pathfinding"
+            className="btn btn-outline-light"
+          >
             Pathfinding visualization
           </a>
         </div>
