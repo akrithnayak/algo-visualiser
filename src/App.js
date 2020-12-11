ec7d1d734f1aa858054d73e7b5c5c2ffdb2f2c28
Akrith H Nayak <akrithnayak2000@gmail.com>
2020-12-11 20:06:55 +0530

    Added deploy configuration

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import SortingVisualiser from "./Sorting/Sorting";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Switch, Route, Redirect, HashRouter } from "react-router-dom";
+import { Switch, Route, Redirect, BrowserRouter } from "react-router-dom";
 import PathFindingVisualiser from "./Pathfinding/Pathfinding";
 import Home from "./Home";
 
 function App() {
   console.log(process.env.REACT_APP_PUBLIC_URL);
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Switch>
-        <Route path="/algo-visualiser/sorting" exact>
+        <Route path="/sorting" exact>
           <SortingVisualiser />
         </Route>
-        <Route path="/algo-visualiser/pathfinding" exact>
+        <Route path="/pathfinding" exact>
           <PathFindingVisualiser />
         </Route>
         <Route path="/">
@@ -20,7 +20,7 @@ function App() {
         </Route>
         <Redirect to="/" />
       </Switch>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
